516a5387ba0da24e99be02b79e14f88ba10f417d
Deniz Erdem <deniz7erdem@icloud.com>
2024-05-11 14:12:04 +0300

    set api property for swagger

--- server/src/auth/dto/login.dto.ts
+++ server/src/auth/dto/login.dto.ts
@@ -1,8 +1,11 @@
-import { IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
-export class loginDto{
-    mail: string;
-    
-    @IsString()
-    password: string;
+export class loginDto {
+  @ApiProperty()
+  mail: string;
+
+  @ApiProperty()
+  @IsString()
+  password: string;
 }
--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(8)
   @MaxLength(20)
